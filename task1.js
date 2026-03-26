edffd518041386bc79de6ee93cdd6c889b92c9d2
Retostephen <retostephen2006@gmail.com>
2026-03-26 13:51:08 +0100

    Removed all the recently opened files and then created a html file because I misinterpreted the question structure

--- task1.js
+++
@@ -1,8 +0,0 @@
-// 1. Create an initials generator program
-
-// - Ask for full name
-// - Output initials
-
-// Example:
-// Andrew Solex → A.S
-
--- task2.js
+++
@@ -1,5 +0,0 @@
-// 2. Create a word position finder
-
-// - Ask for a sentence
-// - Ask for a letter
-// - Show the position of that letter
